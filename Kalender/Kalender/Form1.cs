9aefaedcc23ccd423eb472bf4f391f75cab03134
Naadow <Tim.lyra@gmx.net>
2019-12-10 20:56:08 +0100

    -School 8.12.19

---
+++ Kalender/Kalender/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Kalender
+{
+    public partial class Form1 : Form
+    {
+        private const string Error = "UPS!\n" + "Something's Wrong!";
+
+        private const string Success = "Successfully \n " +
+                                 "Registred";
+        public Form1()
+        {
+            InitializeComponent();
+            pnl_MainMenue.Hide();
+            pnl_loggedin.Hide();
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Label8_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pnl_Register.Hide();
+            pnl_SignUp.Show();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pnl_SignUp.Hide();
+            pnl_Register.Show();
+        }
+
+        private void Btn_Register_Register_Click(object sender, EventArgs e)
+        {
+            var queryclass = new Query();
+            var passwordclass = new PasswordControll();
+            var emailClass = new EmailContorll();
+            var errorclass = new ErrorMessanges();
+
+            bool allentered = errorclass.allEntered(bMT_Email.Text, bMT_Password_Register.Text, bMT_userName_Register.Text,
+                bMT_RePassword_Register.Text);
+
+            bool controll = passwordclass.samePassword(bMT_Password_Register.Text, bMT_RePassword_Register.Text);
+
+            bool emailcontroll = emailClass.conrtollemail(bMT_Email.Text);
+
+            switch (emailcontroll)
+            {
+                case false when controll == true && allentered==true :
+                {
+                    var password = passwordclass.HashPassword(bMT_Password_Register.Text);
+                    queryclass.insertinto(bMT_userName_Register.Text, password, bMT_Email.Text);
+                    pb_error_Register.Visible = false;
+                    pb_success_Register.Visible = true;
+                    lbl_Status_Register.Text = Success;
+                    break;
+                }
+                case false:
+                {
+                    pb_error_Register.Visible = true;
+                    lbl_Status_Register.Text = Error;
+                    break;
+                }
+                case true:
+                    pb_error_Register.Visible = true;
+                    lbl_Status_Register.Text = Error ;
+                    break;
+            }
+        }
+
+        private void btn_SignUp_SignUp_Click(object sender, EventArgs e)
+        {
+            var signUpClass = new SignUp();
+
+            if (signUpClass.controllData(bMT_userName_SignUp.Text, bMT_Password_SignUp.Text)==true)
+            {
+                pb_error_SignUp.Visible = false;
+                pb_success_SignUp.Visible = false;
+                lbl_Status_SignUp.Text ="LOGGED IN!";
+                pnl_notloggedin.Hide();
+                pnl_MainMenue.Show();
+                pnl_loggedin.Show();
+            }
+
+            else
+            {
+                pb_error_SignUp.Visible = true;
+                lbl_Status_SignUp.Text = Error;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
---
+++ Kalender/Kalender/UserColums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+namespace Kalender
+{
+    class UserColums
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public string DBConnectionString = "datasource = 127.0.0.1; port=3306;username=root;password=;database=calenderdb";
+        
+        public MySqlConnection DatabaseConnection { get;  set; }
+        public MySqlCommand CommandDatabase { get; set; }
+
+        public void DBConnection(string sqlcommmand)
+        {
+            DatabaseConnection = new MySqlConnection(DBConnectionString);
+            CommandDatabase = new MySqlCommand(sqlcommmand, DatabaseConnection);
+            CommandDatabase.CommandTimeout = 60;
+        }
+
+    }
+}
